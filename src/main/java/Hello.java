813b9d2e7023e450389916a80a91ab6c1de3d104
sahemr <sahinemre810@gmail.com>
2021-06-10 14:50:28 +0200

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,9 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello");
-
         System.out.println("Developing hello feature");
-
-
+        System.out.println("Local Repo is already published to GitHup");
+        System.out.println("Doing more work");
     }
 }
